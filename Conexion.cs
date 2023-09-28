9f597742b08c922c9a947e504e98f6a8ccca07e0
FRANCO ISMAEL QUEZADA LORCA <FRANCO.QUEZADAL@correoaiep.cl>
2023-09-28 19:57:37 -0300

    fusion: inicio lol

---
+++ Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyDB
+{
+    internal class Conexion
+    {
+        public static string Conec = "server=LENOVO-IDEAPAD\\SQLEXPRESS;database=Supermercado;uid =sa; pwd =admin_123";
+    }
+    /* 
+      Franco: public static string Conec = "server=LENOVO-IDEAPAD\SQLEXPRESS;database=Supermercado;uid =sa; pwd =12345678";
+      Anibal: 
+      Rafael: 
+      Monserrat: 
+    */
+}
